dff18fb9c66f421388128ac8077707b1833fea02
John-Baptiste <ocyrus.johnbaptiste@fiserv.com>
2023-10-31 14:25:14 -0400

    ah5 done

--- Solution.java
+++ Solution.java
@@ -1,7 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
+    /**
+     * @param s
+     * @return
+     */
     public static char mostFrequentChar(String s) {
-      
-      return;
+      //create hash-map to store char duplicates
+      final Map<Character, Integer> map = new HashMap<Character, Integer>();
+      int count = 0;
+      char sum = 0;
+      //loop through the given string 
+      for (int i = 0; i < s.length(); i ++) {
+      //store character in map and add duplicates 
+      char c = s.charAt(i);
+      map.put(c, map.getOrDefault(c, 0)+1);
+      if (count < map.get(c)) {
+        sum = c;
+        count = map.get(c);
+      }
+      }
+      System.out.println(sum+": "+count);
+      return sum;
     }
   
     public static void main(String[] args) {
